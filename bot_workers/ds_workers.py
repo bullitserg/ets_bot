139b58a1eb165674e94e493592c6c95e1a9c5f04
belims <bullit88@mail.ru>
2018-12-03 13:32:06 +0300

    Небольшие исправления

--- bot_workers/ds_workers.py
+++ bot_workers/ds_workers.py
@@ -6,7 +6,7 @@ from ets.ets_excel_creator import Excel
 from config import ds_status_dir, ds_status_by_guid_dir, ds_commission_dir
 from bot_functions.other_functions import write_ds_log
 from config import ds_log_file
-from os.path import normpath
+from os.path import normpath, getsize
 import re
 import ets.ets_ds_lib as ds
 from bot_functions.bot_functions import dialog
@@ -307,13 +307,13 @@ def callback_check_ds_log(bot, update, user_data):
             [re.findall(r'(^.*?) #', l)[0] for l in sorted(o_ds_log.readlines()[-3:], reverse=True)]
         )
 
-    if bot_answer:
+    if getsize(normpath(ds_log_file)):
         bot.send_document(chat_id=chat_id,
                           document=open(normpath(ds_log_file), 'rb'),
                           caption='Сведения о платежных операциях')
 
     else:
-        bot_answer += 'Сведения о событиях отсутствуют'
+        bot_answer = 'Сведения о событиях отсутствуют'
 
     worker_logger(username, chat_id, text=bot_answer)
     send_message_by_parts(bot, chat_id, bot_answer)
